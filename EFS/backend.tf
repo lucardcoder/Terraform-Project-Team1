12f2b78ae9c36a1fb2ef7e447916ecebf3acfc5b
Alp Kocer <104270411+lucardcoder@users.noreply.github.com>
2022-12-10 20:18:04 -0500

    Update backend.tf

--- EFS/backend.tf
+++ EFS/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tfstate-****"          #your bucket name. ex : tfstate-{account_id}
     dynamodb_table = "tfstate-team1"           #your dynamodb table name
-    key            = "tfstate-team1/dev/EFS" #your dynamodb key value
+    key            = "tfstate-team1/dev/EFS" 
     region         = "us-east-1"
     encrypt        = true
   }
